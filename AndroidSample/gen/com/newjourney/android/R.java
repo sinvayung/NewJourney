d07bd3ea04d63e4bd92510581e8039bde99fcbe8
stanyung <stanyung@163.com>
2013-09-01 01:05:06 +0800

    属性动画

--- AndroidSample/gen/com/newjourney/android/R.java
+++ AndroidSample/gen/com/newjourney/android/R.java
@@ -10,6 +10,16 @@ package com.newjourney.android;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int color0=0x7f040000;
+        public static final int color1=0x7f040001;
+        public static final int color2=0x7f040002;
+        public static final int color3=0x7f040003;
+        public static final int color4=0x7f040004;
+        public static final int color5=0x7f040005;
+        public static final int color6=0x7f040006;
+        public static final int color7=0x7f040007;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +27,41 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int box=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f09000a;
+        public static final int box=0x7f090001;
+        public static final int destop=0x7f090000;
+        public static final int frameLayout=0x7f090002;
+        public static final int scrollView1=0x7f090003;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090005;
+        public static final int textView3=0x7f090006;
+        public static final int textView4=0x7f090007;
+        public static final int textView5=0x7f090008;
+        public static final int textView6=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int common_test=0x7f030001;
+        public static final int object_animation_sample=0x7f030002;
+        public static final int scrollview_sample=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int row_content=0x7f060004;
+        public static final int row_title=0x7f060003;
     }
     public static final class style {
         /** 
@@ -59,10 +85,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- AndroidSample/src/com/newjourney/android/MainActivity.java
+++ AndroidSample/src/com/newjourney/android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.newjourney.android;
 
+import com.newjourney.android.animation.ObjectAnimationSample;
+import com.newjourney.android.view.ScrollViewSample;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +14,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Intent intent = new Intent(this, ScrollViewSample.class);
+		intent = new Intent(this, ObjectAnimationSample.class);
+		startActivity(intent);
+		this.finish();
+		
 	}
 
 	@Override
