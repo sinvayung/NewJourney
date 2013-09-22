969ce8f36e40020bd38c302b8de8e0c2f66f4e74
StanYung <stanyung@163.com>
2013-09-22 15:20:50 +0800

    Clear

--- AndroidSample/src/com/newjourney/android/MainActivity.java
+++ AndroidSample/src/com/newjourney/android/MainActivity.java
@@ -1,11 +1,7 @@
 package com.newjourney.android;
 
-import com.newjourney.android.animation.ObjectAnimationSample;
-import com.newjourney.android.view.ScrollViewSample;
-
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -14,17 +10,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Intent intent = new Intent(this, ScrollViewSample.class);
-		intent = new Intent(this, ObjectAnimationSample.class);
-		startActivity(intent);
-		this.finish();
-		
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
